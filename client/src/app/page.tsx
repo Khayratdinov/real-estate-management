becce2773deb8e50551177610247cd1800c47502
khayratdinov <khayratdinov.np@gmail.com>
2024-10-01 22:22:30 +0500

    chore: 🛠️ setup custom fonts, ESLint, and Prettier

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,10 @@
-
-
 export default function Home() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1 className="font-robotoSlab text-6xl ">Home Page!!!!</h1>
+			<p className="font-openSans text-3xl">
+				There was this one dog that was too greedy
+			</p>
+		</div>
+	);
 }
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -14,7 +14,12 @@ const config: Config = {
   			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
   		},
   		borderRadius: {},
-  		colors: {}
+  		colors: {},
+		fontFamily: {
+			openSans: ["var(--font-openSans)"],
+			robotoSlab: ["var(--font-robotoSlab)"],
+		},
+		
   	}
   },
   plugins: [require("tailwindcss-animate")],
