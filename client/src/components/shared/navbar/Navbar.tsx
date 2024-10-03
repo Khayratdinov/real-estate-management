8cbc0c08d1c4f076afa1adb1e574c448225e4805
khayratdinov <khayratdinov.np@gmail.com>
2024-10-03 20:23:17 +0500

    feat: ✨ add theme switcher component

--- client/src/components/shared/navbar/Navbar.tsx
+++ client/src/components/shared/navbar/Navbar.tsx
@@ -1,5 +1,7 @@
 import { HomeModernIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
+import ThemeSwitcher from "./ThemeSwitcher";
+
 export default function Navbar() {
 	return (
 		<nav className="flex-between bg-baby_rich border-b-platinum shadow-platinum fixed z-50 w-full gap-5 border-b-2 p-4 sm:p-6 lg:px-12 dark:border-b-0 dark:shadow-none">
@@ -9,11 +11,10 @@ export default function Navbar() {
 					Alpha <span className="text-lime-500"> Apartments</span>
 				</p>
 			</Link>
+
 			<div className="flex items-center gap-4 sm:gap-6 lg:gap-8">
 				{/* placeholder theme switcher component */}
-				<div className="dark:text-pumpkin hidden text-lg sm:block sm:text-xl">
-					Theme Switcher
-				</div>
+				<ThemeSwitcher />
 				{/* placeholder theme switcher component */}
 				<div className="dark:text-pumpkin text-lg sm:text-xl">MobileNavbar</div>
 			</div>
