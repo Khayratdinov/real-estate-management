6329106a45b4dd45180955219498b59ca039ae9a
khayratdinov <khayratdinov.np@gmail.com>
2024-10-07 23:19:27 +0500

    feat: ✨ setup slices, reducers, and provider

--- client/src/app/(estate)/layout.tsx
+++ client/src/app/(estate)/layout.tsx
@@ -24,3 +24,5 @@ export default function layout({ children }: LayoutProps) {
 		</main>
 	);
 }
+
+// npm i react-hook-form@7.50.1 @hookform/resolvers@3.3.4 zod@3.22.4 react-toastify@10.0.4 react-redux@9.1.0 @reduxjs
