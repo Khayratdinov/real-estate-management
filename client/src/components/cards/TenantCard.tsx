7d11d6c8dec0d774f303d888a0c6e826f04e9387
khayratdinov <khayratdinov.np@gmail.com>
2024-10-31 16:44:42 +0500

    feat: ✨ Add tenants search functionality

--- client/src/components/cards/TenantCard.tsx
+++ client/src/components/cards/TenantCard.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useGetAllUsersQuery } from "@/lib/redux/features/users/usersApiSlice";
 import { useTheme } from "next-themes";
+
 import React from "react";
 import Spinner from "../shared/Spinner";
 import UsersSearch from "../shared/search/UsersSearch";
@@ -23,9 +24,13 @@ import {
 import { formatDate } from "@/utils";
 import ProtectedRoute from "../shared/ProtectedRoutes";
 import { Avatar, AvatarImage } from "../ui/avatar";
+import { useAppSelector } from "@/lib/redux/hooks/typedHooks";
+
 function TenantCardContent() {
 	const { theme } = useTheme();
-	const { data, isLoading } = useGetAllUsersQuery({});
+	const searchTerm = useAppSelector((state) => state.user.searchTerm);
+	const { data, isLoading } = useGetAllUsersQuery({ searchTerm });
+
 	if (isLoading) {
 		return (
 			<div className="flex-center pt-32">
@@ -33,19 +38,21 @@ function TenantCardContent() {
 			</div>
 		);
 	}
+
 	return (
 		<div>
 			<UsersSearch />
 			<h1 className="flex-center font-robotoSlab dark:text-pumpkin text-4xl sm:text-5xl">
 				All Tenants - ({data?.profiles.results.length})
 			</h1>
+
 			<div className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
 				{data && data.profiles.results.length > 0 ? (
 					data.profiles.results.map((tenant) => (
 						<Card key={tenant.id}>
 							<CardContent className="rounded-lg p-4">
 								<CardHeader className="flex-col-center text-center">
-									<Avatar className="border-pumpkin mx-auto size-32 overflow-hidden rounded-full border-4 object-cover">
+									<Avatar className="border-pumpkin mx-auto size-28 overflow-hidden rounded-full border-4 object-cover">
 										<AvatarImage
 											alt="User profile avatar"
 											src={
@@ -54,8 +61,6 @@ function TenantCardContent() {
 													? "/assets/icons/user-profile-circle.svg"
 													: "/assets/icons/user-profile-light-circle.svg")
 											}
-											width={200}
-											height={200}
 										/>
 									</Avatar>
 									<CardTitle className="h3-semibold font-robotoSlab dark:text-platinum">
@@ -115,6 +120,7 @@ function TenantCardContent() {
 		</div>
 	);
 }
+
 export default function TenantCard() {
 	return (
 		<ProtectedRoute>
