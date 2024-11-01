0849530748f05ba2d3d63161c79c8503fec8d585
khayratdinov <khayratdinov.np@gmail.com>
2024-11-01 19:06:47 +0500

    feat: ✨ Add tenants pagination component

--- client/src/lib/redux/features/users/userSlice.ts
+++ client/src/lib/redux/features/users/userSlice.ts
@@ -1,7 +1,10 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
 const initialState = {
 	searchTerm: "",
+	page: 1,
 };
+
 const userSlice = createSlice({
 	name: "user",
 	initialState,
@@ -9,7 +12,11 @@ const userSlice = createSlice({
 		setSearchTerm: (state, action: PayloadAction<string>) => {
 			state.searchTerm = action.payload;
 		},
+		setCurrentPage: (state, action: PayloadAction<number>) => {
+			state.page = action.payload;
+		},
 	},
 });
-export const { setSearchTerm } = userSlice.actions;
+
+export const { setSearchTerm, setCurrentPage } = userSlice.actions;
 export default userSlice.reducer;
